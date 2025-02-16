182c9957787197d28eec94b1f81593f8905e5276
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2025-02-15 17:45:52 -0800

    tried to make collision work

--- Assets/Scripts/talk.cs
+++ Assets/Scripts/talk.cs
@@ -1,28 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class talk : MonoBehaviour
 {
-     public Text text;
+     public TextPrinter DarwinIntro;
+     public TextPrinter Quest;
+     public TextPrinter Credentials;
+     public TextPrinter AmnesiaInfo;
+
+     bool inRange = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        text.gameObject.SetActive(false)
+        DarwinIntro.gameObject.SetActive(false);
+        Quest.gameObject.SetActive(false);
+        Credentials.gameObject.SetActive(false);
+        AmnesiaInfo.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inRange && Input.GetKeyDown(KeyCode.Space))
+        {
+            DarwinIntro.gameObject.SetActive(true);
+        }
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("enter");
         // Check if the duck should interact witha Player
-        if ((collision.gameObject.CompareTag("Player"))&& Input.GetKeyDown(KeyCode.Space))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            text.gameObject.SetActive(true);
+            inRange = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("exit");
+        // Check if the duck should interact witha Player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            inRange = false;
         }
     }
 }
