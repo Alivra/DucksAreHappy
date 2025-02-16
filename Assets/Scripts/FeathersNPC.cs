5bdbff9f781d1264d33a9a3d94a1c53f49c712a7
Alivra <itsalivra@gmail.com>
2025-02-16 14:17:01 -0800

    some changes

--- Assets/Scripts/FeathersNPC.cs
+++ Assets/Scripts/FeathersNPC.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NPC : MonoBehaviour
+public class FeathersNPC : MonoBehaviour
 {
     public GameObject dialoguePanel;
     public Text dialogueText;
