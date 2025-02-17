b1a8dc281a3d8545b5696adcf28ff6cbac4d0fe5
Alivra <itsalivra@gmail.com>
2025-02-16 16:40:09 -0800

    all the ui for sir quacks is done

--- Assets/Scripts/SirQuacksNPC.cs
+++ Assets/Scripts/SirQuacksNPC.cs
@@ -14,8 +14,24 @@ public class SirQuacksNPC : MonoBehaviour
     public float wordSpeed;
     public bool playerIsClose;
 
-    private bool finishedConversation;
+    private bool finishedConversation = false;
+    private bool disgraced = false;
+
     public GameObject choice1;
+    public GameObject choice2;
+    public GameObject choice3;
+    public GameObject choice4;
+
+    public GameObject answer1;
+    public GameObject answer2;
+    public GameObject answer3;
+    public GameObject answer4;
+    public GameObject answer5;
+    public GameObject answer6;
+    public GameObject answer7;
+    public GameObject answer8;
+    public GameObject answer9;
+
 
     void Start()
     {
@@ -30,9 +46,15 @@ public class SirQuacksNPC : MonoBehaviour
             {
                 zeroText();
             }
+            else if (disgraced)
+            {
+                index = 19;
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
             else if (finishedConversation)
             {
-                index = dialogue.Length - 1;
+                index = 21;
                 dialoguePanel.SetActive(true);
                 StartCoroutine(Typing());
             }
@@ -48,6 +70,12 @@ public class SirQuacksNPC : MonoBehaviour
             if (index == 1)
             {
                 choice1.SetActive(true);
+                choice2.SetActive(true);
+            }
+            else if (index == 19)
+            {
+                choice3.SetActive(true);
+                choice4.SetActive(true);
             }
             else
             {
@@ -75,9 +103,293 @@ public class SirQuacksNPC : MonoBehaviour
     public void NextLine()
     {
         choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
         contButton.SetActive(false);
 
-        if (index < dialogue.Length - 3)
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        if (FindNextEmptyIndex(dialogue) == -1)
+        {
+            if (index < dialogue.Length - 1)
+            {
+                index++;
+                dialogueText.text = "";
+                StartCoroutine(Typing());
+            }
+            else
+            {
+                zeroText();
+            }
+        }
+        else
+        {
+            if (index < FindNextEmptyIndex(dialogue))
+            {
+                index++;
+                dialogueText.text = "";
+                StartCoroutine(Typing());
+            }
+            else
+            {
+                zeroText();
+            }
+        }
+    }
+
+    int FindNextEmptyIndex(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[index] == "") // Check for empty or null values
+            {
+                return i;
+            }
+        }
+        return -1; // Return -1 if no empty index is found
+    }
+
+    public void goToChoice1()
+    {
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
+        contButton.SetActive(false);
+
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        index = 2;
+
+        if (index < 7)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            choice3.SetActive(true);
+            choice4.SetActive(true);
+        }
+
+    }
+
+    public void goToChoice2()
+    {
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
+        contButton.SetActive(false);
+
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        index = 7;
+
+        if (index < 10)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+
+        }
+        else
+        {
+            choice3.SetActive(true);
+            choice4.SetActive(true);
+        }
+    }
+
+    public void goToChoice3()
+    {
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
+        contButton.SetActive(false);
+
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        index = 20;
+
+        if (index < 20)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+
+        }
+        else
+        {
+            zeroText();
+            disgraced = true;
+        }
+
+    }
+
+    public void goToChoice4()
+    {
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
+        contButton.SetActive(false);
+
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        index = 12;
+
+        if (index < 12)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            answer1.SetActive(true);
+            answer2.SetActive(true);
+            answer3.SetActive(true);
+
+        }
+    }
+
+    public void goToRightAnswer1()
+    {
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
+
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        contButton.SetActive(false);
+
+        index = 14;
+
+        if (index < 14)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            answer4.SetActive(true);
+            answer5.SetActive(true);
+            answer6.SetActive(true);
+
+        }
+    }
+
+    public void goToRightAnswer2()
+    {
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
+
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        contButton.SetActive(false);
+
+        index = 16;
+
+        if (index < 16)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            answer7.SetActive(true);
+            answer8.SetActive(true);
+            answer9.SetActive(true);
+
+        }
+    }
+
+    public void goToRightAnswer3()
+    {
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
+
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        contButton.SetActive(false);
+
+        index = 18;
+
+        if (index < 18)
         {
             index++;
             dialogueText.text = "";
@@ -86,7 +398,42 @@ public class SirQuacksNPC : MonoBehaviour
         else
         {
             zeroText();
+            disgraced = false;
             finishedConversation = true;
+        }
+    }
+
+    public void goToWrongAnswer()
+    {
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        choice3.SetActive(false);
+        choice4.SetActive(false);
+
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+        answer1.SetActive(false);
+
+        contButton.SetActive(false);
+
+        index = 20;
+
+        if (index < 20)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+            disgraced = true;
         }
     }
 
