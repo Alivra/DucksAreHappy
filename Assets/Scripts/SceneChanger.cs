769c9e2fb91788870a811638b25ea06b3d75e2a4
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2025-02-16 17:11:03 -0800

    added psyduck

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -3,43 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour {
-    
+public class SceneChanger : MonoBehaviour
+{
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+    public void LoadScene(string sceneName, float delay)
+    {
+        StartCoroutine(LoadSceneAfterDelay(sceneName, delay));
+    }
 
+    private IEnumerator LoadSceneAfterDelay(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
 
-    public void GoToCredits() {
+    public void GoToCredits()
+    {
         SceneManager.LoadScene("CreditsScene");
 
     }
 
-    public void GoToTitle() {
+    public void GoToTitle()
+    {
         SceneManager.LoadScene("TitleScene");
 
     }
 
-    public void GoToGame() {
+    public void GoToGame()
+    {
         SceneManager.LoadScene("SampleScene");
 
     }
 
-    public void GoToShredE() {
+    public void GoToShredE()
+    {
         SceneManager.LoadScene("shredE");
     }
 
-    public void GoToShredI() {
+    public void GoToShredI()
+    {
         SceneManager.LoadScene("shredI");
     }
 
-    public void GoToLeave() {
+    public void GoToLeave()
+    {
         SceneManager.LoadScene("Leave");
 
     }
 
-    public void GoToEat() {
+    public void GoToEat()
+    {
         SceneManager.LoadScene("Eat");
 
     }
